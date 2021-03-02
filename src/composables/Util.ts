3e0815becab15de4a7e7347a088b532eb4d3ccfe
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-02 14:47:12 +0100

    Composable: Util

---
+++ src/composables/Util.ts
@@ -0,0 +1,32 @@
+export default () => {
+  const normalizeFrecuencias = (frecuencia: string): string => {
+    switch (frecuencia) {
+      case 'LV': {
+        return 'Lunes a Viernes';
+      }
+      case 'SD': {
+        return 'Sábados y Domingos';
+      }
+      case 'F': {
+        return 'Festivos';
+      }
+      default: {
+        console.log(`No se reconoce ${frecuencia}`);
+        return '';
+      }
+    }
+  }
+  
+  const hoursToMinutes = (hour: string): number => {
+    const arr = hour.split(':');
+    const hourInMinutes = parseInt(arr[0], 10) * 60;
+    const minutes = parseInt(arr[1], 10);
+  
+    return hourInMinutes + minutes;
+  }
+  
+  return {
+    normalizeFrecuencias,
+    hoursToMinutes,
+  }
+};
