b781b032d4ec0082b597be6ac8c5845da6225848
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-02 14:46:04 +0100

    Componente principal

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <header class="header">
+      <h1 class="logo" routerLink="/">
+        Autobuses
+      </h1>
+      <nav class="nav">
+        <router-link class="nav-link" to="/nucleos">
+          Núcleos Urbanos
+        </router-link>
+        <router-link class="nav-link" to="/lineas">
+          Líneas
+        </router-link>
+      </nav>
+    </header>
+
+    <main class="container main">
+      <router-view />
+    </main>
   </div>
-  <router-view/>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
