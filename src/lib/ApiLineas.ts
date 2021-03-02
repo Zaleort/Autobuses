936a63ab2f7b7a14acb7dc9439b15a29eb9a0a70
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-02 18:46:17 +0100

    Fix: Typescript errors en Lib Api

--- src/lib/ApiLineas.ts
+++ src/lib/ApiLineas.ts
@@ -2,12 +2,12 @@ import { ApiLinea } from '@/interfaces/apiResponses';
 import api from '@/lib/ApiService';
 import store from '@/store';
 
-const urlBase = store.state.urlBase;
+const { urlBase } = store.state;
 
 export default {
   getLineas: async (): Promise<ApiLinea[]> => api.get(`${urlBase}lineas`),
   getLinea: async (id: string): Promise<ApiLinea> => {
     if (!id) return Promise.reject();
     return api.get(`${urlBase}lineas/${id}`);
-  }
-}
+  },
+};
--- src/lib/ApiNucleos.ts
+++ src/lib/ApiNucleos.ts
@@ -2,12 +2,12 @@ import { ApiNucleo } from '@/interfaces/apiResponses';
 import api from '@/lib/ApiService';
 import store from '@/store';
 
-const urlBase = store.state.urlBase;
+const { urlBase } = store.state;
 
 export default {
-  getNucleos: async (): Promise<ApiNucleo[]> => api.get(`${urlBase}Nucleos`),
+  getNucleos: async (): Promise<ApiNucleo[]> => api.get(`${urlBase}nucleos`),
   getNucleo: async (id: string): Promise<ApiNucleo> => {
     if (!id) return Promise.reject();
     return api.get(`${urlBase}nucleos/${id}`);
-  }
-}
+  },
+};
