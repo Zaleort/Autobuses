9ee8c457e99faf988a7920522cee6fb90b593de5
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-03 13:09:46 +0100

    Componente Tooltip

---
+++ src/components/ui/UiTooltip.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tooltip__parent" :style="{ display }">
+    <span
+      :class="{
+        'tooltip': true,
+        [`ui-tooltip--${color}`]: true,
+        [`ui-tooltip--${position}`]: true,
+      }"
+    >
+      <slot name="content">{{ content }}</slot>
+    </span>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UiTooltip',
+  props: {
+    content: {
+      type: String,
+      default: null,
+    },
+
+    color: {
+      type: String,
+      default: 'black',
+    },
+
+    position: {
+      type: String,
+      default: 'top',
+      validator: (val: string) => ['top', 'bottom'].indexOf(val) !== -1,
+    },
+
+    display: {
+      type: String,
+      default: 'inline-block',
+    },
+  },
+});
+</script>
