1fee7469939fcf19c6d7262998dfed5e800e0c85
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-05 17:59:36 +0100

    Filtrar horarios por paradas

--- src/components/LineaHorario.vue
+++ src/components/LineaHorario.vue
@@ -1,22 +1,24 @@
 <template>
-  <div>
+  <div v-if="showHorario">
     <h2 :id="tipoHorario">
       Horarios de {{ tipoHorario }}
       <ui-icon
         v-if="verHorarios"
+        class="clickable"
         icon="minus"
         size="mini"
         @click="toggleHorario"
       />
       <ui-icon
         v-else
+        class="clickable"
         icon="plus"
         size="mini"
         @click="toggleHorario"
       />
     </h2>
     <div
-      v-for="(horario, index) of horarios"
+      v-for="(horario, index) of filteredHorarios"
       v-show="verHorarios"
       :key="index"
     >
@@ -27,12 +29,14 @@
         {{ normalizeFrecuencias(horario.frecuencia) }}
         <ui-icon
           v-if="verFrecuencia[horario.frecuencia]"
+          class="clickable"
           icon="minus"
           size="small"
           @click="toggleFrecuencia(horario.frecuencia)"
         />
         <ui-icon
           v-else
+          class="clickable"
           icon="plus"
           size="small"
           @click="toggleFrecuencia(horario.frecuencia)"
@@ -41,14 +45,14 @@
       <div
         v-for="parada of horario.paradas"
         v-show="verFrecuencia[horario.frecuencia]"
-        :key="parada._id"
+        :key="parada.id"
         class="linea-horario__parada"
       >
         <div class="linea-horario__info">
           <h4>{{ parada.name }}</h4>
           <span :class="'zona-' + parada.zona">Zona {{ parada.zona }}</span>
         </div>
-        <span>
+        <div class="linea-horario">
           <span
             v-for="(hora, index) of parada.horario"
             :key="index"
@@ -57,16 +61,20 @@
             <span v-if="hora.excepcion" class="linea-horario__excepcion">{{ hora.excepcion }}</span>
             <span class="linea-horario__hora">{{ hora.hora }}</span>
           </span>
-        </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue';
+import {
+  computed,
+  defineComponent, PropType, reactive, ref, watch,
+} from 'vue';
 import UiIcon from '@/components/ui/UiIcon.vue';
 import Util from '@/composables/Util';
+import { useStore } from '@/store';
 
 export default defineComponent({
   name: 'LineaHorario',
@@ -81,23 +89,62 @@ export default defineComponent({
     },
 
     horarios: {
-      type: Array,
+      type: Array as PropType<TablaHorarios[]>,
       default: () => [],
     },
 
     filtro: {
-      type: Object,
+      type: Object as PropType<LineaFiltro>,
       default: () => ({ salida: null, destino: null }),
     },
   },
 
-  setup() {
+  setup(props) {
+    const store = useStore();
+    const paradas = computed(() => {
+      if (props.tipoHorario === 'ida') return store.state.linea.paradasIda;
+      return store.state.linea.paradasVuelta;
+    });
+
     const { normalizeFrecuencias } = Util();
     const verHorarios = ref(true);
+    const showHorario = ref(true);
     const verFrecuencia = reactive({
       LV: true,
       SD: true,
       F: true,
+    });
+
+    const filteredHorarios = ref<TablaHorarios[]>(props.horarios);
+
+    watch(() => props.filtro, () => {
+      if (!props.filtro.salida || !paradas.value) {
+        filteredHorarios.value = props.horarios;
+        showHorario.value = true;
+        return;
+      }
+
+      const { salida, destino } = props.filtro;
+      const salidaIndex = paradas.value.findIndex(p => p === salida);
+      const destinoIndex = paradas.value.findIndex(p => p === destino);
+
+      console.log(salidaIndex);
+      console.log(destinoIndex);
+
+      if (destinoIndex >= 0 && salidaIndex >= destinoIndex) {
+        showHorario.value = false;
+        return;
+      }
+
+      showHorario.value = true;
+      filteredHorarios.value = JSON.parse(JSON.stringify(props.horarios));
+      filteredHorarios.value.forEach(h => {
+        const p = h.paradas;
+        h.paradas = [];
+        h.paradas.push(p[salidaIndex]);
+
+        if (destinoIndex >= 0) h.paradas.push(p[destinoIndex]);
+      });
     });
 
     const toggleHorario = (): boolean => {
@@ -111,7 +158,11 @@ export default defineComponent({
     };
 
     return {
+      store,
+      paradas,
+      filteredHorarios,
       normalizeFrecuencias,
+      showHorario,
       verHorarios,
       verFrecuencia,
       toggleHorario,
