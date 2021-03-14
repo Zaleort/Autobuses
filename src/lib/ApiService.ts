0f648b8e8cf5806527ba8e9578acdc9b270055f7
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-14 03:18:20 +0100

    Devolver promesa rechazada cuando no se recibe un 200

--- src/lib/ApiService.ts
+++ src/lib/ApiService.ts
@@ -1,14 +1,41 @@
 export default {
   async get(url: string) {
     const response = await fetch(url);
+
+    if (!response.ok) {
+      const json = await response.json();
+      const error = {
+        status: response.status,
+        statusText: response.statusText,
+        message: json.message,
+      };
+
+      return Promise.reject(error);
+    }
+
     return response.json();
   },
 
   async post(url: string, body: any) {
     const response = await fetch(url, {
       method: 'POST',
-      body,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(body),
     });
+
+    if (!response.ok) {
+      const json = await response.json();
+      const error = {
+        status: response.status,
+        statusText: response.statusText,
+        message: json.message || null,
+      };
+
+      return Promise.reject(error);
+    }
+
     return response.json();
   },
 };
