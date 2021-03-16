24cdbcabb4b001cd6e70003936265fd366dae023
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-16 02:08:44 +0100

    Vista Home para Usuarios e Invitados

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <home-user v-if="usuario" />
+  <home-guest v-else />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import HomeUser from '@/views/HomeUser.vue';
+import HomeGuest from '@/views/HomeGuest.vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HomeUser,
+    HomeGuest,
+  },
+
+  setup() {
+    const store = useStore();
+    const usuario = computed(() => store.state.usuario.usuario);
+
+    return {
+      usuario,
+      store,
+    };
+  },
+});
+</script>
---
+++ src/views/HomeUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="usuario">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+  name: 'HomeUser',
+  setup() {
+    const store = useStore();
+    const usuario = computed(() => store.state.usuario.usuario);
+    const datos = computed(() => store.state.usuario.autobuses);
+
+    return {
+      usuario,
+      datos,
+      store,
+    };
+  },
+});
+</script>
